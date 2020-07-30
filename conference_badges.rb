76fe76678b75f19ab0073c745f532268c5e7d97c
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-30 01:11:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_array = []
+  array.each do |name|
+    badge_array << badge_maker(name)
+  end
+  badge_array
+end
+
+def assign_rooms
+  counter = 0
+  "Hello, #{name}! You'll be assigned to room #{number}!"
+  counter += 1 
+end
+
+def printer
+end
